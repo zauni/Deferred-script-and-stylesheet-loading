3b954d59103f8026b6056b7191933eb0da305f01
Matthias Zaunseder <zaunseder@glanzer-und-partner.de>
2011-05-27 15:22:40 +0200

    onload für css Dateien hinzugefügt
    Allerdings wird der waitingTimeout auch bei css Dateien aufgerufen die schon geladen sind.

--- source/loader.js
+++ source/loader.js
@@ -17,7 +17,8 @@
      *      });
      */
     global.Loader = (function() {
-        var loaded = {};
+        var loaded = {},
+            waitingTime = 5000;
         
         /**
          * Load a stylesheet or a script
@@ -26,8 +27,18 @@
          * @return {Deferred} returns a Deferred object where one can add callbacks
          */
         function load(source) {
+            var prefix,
+                splitted;
+            
             if(typeof source != "string") {
-                $.error("You tried to load something else: '"+(typeof source)+"'!");
+                loaded[source] = new $.Deferred();
+                loaded[source].reject("You tried to load something else: '"+(typeof source)+"'!");
+            }
+            
+            // prefix detected
+            if(source.indexOf("!") > -1 && (splitted = source.split("!"))) {
+                prefix = splitted[0];
+                source = splitted[1];
             }
             
             // if it was loaded before, return the old deferred
@@ -35,14 +46,16 @@
                 return loaded[source];
             }
             
-            if(source.indexOf(".css") > -1) {
-                loaded[source] = loadStyles(source);
+            // check which type we have to load
+            if(prefix == "css" || source.indexOf(".css") > -1) {
+                loaded[source] = loadStyle(source);
             }
-            else if(source.indexOf(".js") > -1) {
+            else if(prefix == "js" || source.indexOf(".js") > -1) {
                 loaded[source] = loadScript(source);
             }
             else {
-                $.error("'" + source + "' is not a known filetype and can not be loaded!");
+                loaded[source] = new $.Deferred();
+                loaded[source].reject("'" + source + "' is not a known filetype and can not be loaded!");
             }
             return loaded[source];
         }
@@ -52,16 +65,56 @@
          * @param {String} source
          * @return {Deferred}
          */
-        function loadStyles(source) {
-            var deferred = new $.Deferred();
+        function loadStyle(source) {
+            var deferred = new $.Deferred(),
+                link = $("<link />", {
+                    rel: "stylesheet",
+                    type: "text/css",
+                    href: source
+                }),
+                callback = function() {
+                    deferred.resolve();
+                };
                 
-            $("<link />", {
-                rel: "stylesheet",
-                type: "text/css",
-                href: source
-            }).appendTo("head");
+            link.appendTo("head");
             
-            deferred.resolve();
+            // onload handler for webkit and gecko - both are not supporting the "onload" event on link elements
+            // 
+            if($.browser.webkit || $.browser.gecko) {
+                (function poll() {
+                    setTimeout(function() {
+                        try {
+                            if(link.sheet.cssRules.length) {
+                                callback();
+                            }
+                            else {
+                                poll();
+                            }
+                        }
+                        catch(exc) {
+                            // If it is a security error than the stylesheet was loaded from a different domain
+                            if((ex.code == 1000) || (ex.message == 'security' || ex.message == 'denied')) {
+                                callback();
+                            }
+                            // otherwise, continue to poll
+                            else {
+                                poll();
+                            }
+                        }
+                    }, 10);
+                })();
+            }
+            else {
+                link[0].onload = callback;
+            }
+            
+            // don't wait forever that a stylesheet is loaded
+            setTimeout(function() {
+                if(!deferred.isResolved()) {
+                    deferred.reject("Could not load '"+source+"' in " + waitingTime/1000 + " seconds.");
+                }
+            }, waitingTime);
+            
             return deferred;
         }
         
@@ -78,7 +131,8 @@
          * Public interface
          */
         return {
-            load: load
+            load: load,
+            waitingTime: waitingTime
         };
     })();
 
