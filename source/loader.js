c7960db4b984d2faa99b3c217b50a2f868d46462
Matthias Zaunseder <zaunseder@glanzer-und-partner.de>
2011-06-06 09:04:26 +0200

    Added more tests. Typing errors are not detected, because cross domain stylesheets are throwing the same exception. JavaScript loading is now much more flexible.

--- source/loader.js
+++ source/loader.js
@@ -16,17 +16,19 @@
      *          var t = new Thing();
      *      });
      */
-    global.Loader = (function() {
-        var loaded = {},
-            waitingTime = 3000;
+    global.Loader = {};
+    (function(loader) {
+        var loaded = {};
+        
+        loader.waitingTime = 1000;
         
         /**
          * Load a stylesheet or a script
          * 
-         * @param {String} source
+         * @param {String} source has to be a JS or CSS file, otherwise use a prefix like: css!stylefromphp.php
          * @return {Deferred} returns a Deferred object where one can add callbacks
          */
-        function load(source) {
+        loader.load = function(source) {
             var prefix,
                 splitted;
             
@@ -58,10 +60,10 @@
                 loaded[source].reject("'" + source + "' is not a known filetype and can not be loaded!");
             }
             return loaded[source];
-        }
+        };
         
         /**
-         * loads a stylesheet into the head
+         * loads a stylesheet
          * @param {String} source
          * @return {Deferred} returns a "Promise" and not the direct deferred, otherwise it could be manipulated
          */
@@ -74,46 +76,47 @@
                 }),
                 callback = function() {
                     deferred.resolve();
-                    console.log("resolved!!", deferred.isResolved(), link);
                 };
                 
             link.appendTo("head");
             
             // onload handler for webkit and gecko - both are not supporting the "onload" event on link elements
-            // 
+            // Inspiration and Code are from these great sources:
+            // https://github.com/SlexAxton/yepnope.js
+            // https://github.com/unscriptable/requirejs/blob/master/require/css.js
+            // http://www.phpied.com/when-is-a-stylesheet-really-loaded/
             if($.browser.webkit || $.browser.mozilla) {
                 (function poll(link) {
-                    setTimeout(function() {
-                        var sheet, rules, ready;
-                        try {
-                            // IE is using "styleSheet" and "rules" but this browser shouldn't
-                            // come that far so these statements are just for added safety
-                            sheet = link.sheet || link.styleSheet;
-                            rules = sheet.cssRules || sheet.rules;
-                            
-                            ready = rules ? rules.length > 0 : rules !== undef;
-                            
-                            console.log(ready);
-                            
-                            if(ready) {
-                                callback();
+                    if(!deferred.isResolved() && !deferred.isRejected()) {
+                        setTimeout(function() {
+                            var sheet, rules, ready;
+                            try {
+                                // IE is using the "styleSheet" and "rules" property but this browser shouldn't
+                                // come that far so these statements are just for added safety
+                                sheet = link.sheet || link.styleSheet;
+                                rules = sheet.cssRules || sheet.rules;
+
+                                ready = rules ? rules.length > 0 : rules !== undef;
+
+                                if(ready) {
+                                    callback();
+                                }
+                                else {
+                                    poll(link);
+                                }
                             }
-                            else {
-                                poll(link);
+                            catch(exc) {
+                                // If it is a security error than the stylesheet was loaded from a different domain
+                                if((exc.code == 1000) || (exc.message == "security" || exc.message == "denied")) {
+                                    callback();
+                                }
+                                // otherwise, continue to poll
+                                else {
+                                    poll(link);
+                                }
                             }
-                        }
-                        catch(exc) {
-                            console.log(exc);
-                            // If it is a security error than the stylesheet was loaded from a different domain
-                            if((exc.code == 1000) || (exc.message == "security" || exc.message == "denied")) {
-                                callback();
-                            }
-                            // otherwise, continue to poll
-                            else {
-                                poll(link);
-                            }
-                        }
-                    }, 30);
+                        }, 30);
+                    }
                 })(link[0]);
             }
             else {
@@ -126,9 +129,9 @@
             // don't wait forever that a stylesheet is loaded
             setTimeout(function() {
                 if(!deferred.isResolved()) {
-                    deferred.reject("Could not load '"+source+"' in " + waitingTime/1000 + " seconds.");
+                    deferred.reject("Could not load '"+source+"' in " + loader.waitingTime/1000 + " seconds.");
                 }
-            }, waitingTime);
+            }, loader.waitingTime);
             
             return deferred.promise();
         }
@@ -139,16 +142,26 @@
          * @return {Deferred}
          */
         function loadScript(source) {
-            return $.getScript(source);
+            // don't return the $.getScript Deferred directly to have more control
+            var tmpDef = new $.Deferred(),
+                realDef = $.getScript(source);
+                
+            realDef.then(function() {
+                tmpDef.resolve();
+            }, function() {
+                tmpDef.reject("Could not load '"+source+"'");
+            });
+            
+            // don't wait forever that a javascript is loaded
+            setTimeout(function() {
+                if(!tmpDef.isResolved()) {
+                    tmpDef.reject("Could not load '"+source+"' in " + loader.waitingTime/1000 + " seconds.");
+                }
+            }, loader.waitingTime);
+                
+            return tmpDef.promise();
         }
         
-        /**
-         * Public interface
-         */
-        return {
-            load: load,
-            waitingTime: waitingTime
-        };
-    })();
+    })(global.Loader);
 
 })(this, this.jQuery);
--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,88 @@
 $(document).ready(function() {
     
+    var jsDef = Loader.load("load/available.js"),
+        cssDef = Loader.load("load/available.css"),
+        crossJsDef = Loader.load("http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"),
+        crossCssDef = Loader.load("http://code.jquery.com/qunit/git/qunit.css"),
+        falseDef = Loader.load("thingy.txt");
+    
+    test("Does the loader return a Deferred Object", function() {
+        notEqual(typeof jsDef, "undefined", "Object found");
+        ok($.isFunction(jsDef.isResolved), "Deferred Object found");
+    });
+    
     test("Check if we can load a css and js file", function() {
-            expect(2);
-            stop();
-            
-            $.when(
-                Loader.load("load/available.js"),
-                Loader.load("load/available.css")
-            ).then(
-                function() {
-                    notEqual(typeof $.fn.foo, "undefined", "Is JavaScript available");
-                    equal($("#css-available-test").css("display"), "inline", "Is CSS available");
-                    start();
-                },
-                function() {
-                    notEqual(typeof $.fn.foo, "undefined", "Is JavaScript available");
-                    equal($("#css-available-test").css("display"), "inline", "Is CSS available");
-                    start();
-                }
-            );
+        expect(2);
+        stop();
+
+        $.when(
+            jsDef,
+            cssDef
+        ).then(
+            function() {
+                notEqual(typeof $.fn.foo, "undefined", "Is JavaScript available");
+                equal($("#css-available-test").css("display"), "inline", "Is CSS available");
+                start();
+            },
+            function() {
+                notEqual(typeof $.fn.foo, "undefined", "Is JavaScript available");
+                equal($("#css-available-test").css("display"), "inline", "Is CSS available");
+                start();
+            }
+        );
+    });
+    
+    test("Check if the state of the Deferred Object can be changed outside of the Loader", function() {
+        ok(!$.isFunction(jsDef.resolve), "Deferred cannot be resolved");
+        ok(!$.isFunction(jsDef.reject), "Deferred cannot be rejected");
+    });
+    
+    test("Check if Deferred can handle cross domain files", function() {
+        expect(2);
+        stop();
+        
+        crossJsDef.done(function() {
+            ok(crossJsDef.isResolved(), "JS Deferred got resolved");
+        });
+        
+        crossCssDef.done(function() {
+            ok(crossCssDef.isResolved(), "CSS Deferred got resolved");
+        });
+        
+        setTimeout(function() {
+            start();
+        }, 1500);
+    });
+    
+    test("Check if Deferred can handle false input filetype", function() {
+        expect(2);
+        stop(500);
+        falseDef.fail(function(msg) {
+            ok(falseDef.isRejected(), "Deferred got rejected");
+            ok(msg.length > 0, "Error message is provided");
+            start();
+        });
+    });
+    
+    test("Check if error timeout is aborting the loading", function() {
+        expect(4);
+        stop();
+        Loader.waitingTime = 0;
+        
+        var def1 = Loader.load("http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.0/themes/base/jquery-ui.css");
+        var def2 = Loader.load("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js");
+        def1.fail(function(msg) {
+            ok(def1.isRejected(), "Deferred got rejected");
+            ok(msg.length > 0, "Error message is provided");
+        });
+        def2.fail(function(msg) {
+            ok(def2.isRejected(), "Deferred got rejected");
+            ok(msg.length > 0, "Error message is provided");
+        });
+        
+        setTimeout(function() {
+            start();
+        }, 500);
     });
 
 });
